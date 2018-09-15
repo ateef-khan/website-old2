1d89657998ce694faf2bfc1aefba09317a494d72
Eric Zhou <ericz1803@gmail.com>
2018-09-15 09:48:56 -0700

    removed unneccesary files

--- src/images/compress.py
+++
@@ -1,7 +0,0 @@
-from glob import iglob
-from PIL import Image
-
-for file in iglob('**/*.jpg', recursive=True):
-    print(file)
-    im = Image.open(file)
-    im.save(file, optimize=True, quality=85)
